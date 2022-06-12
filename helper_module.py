4d9f9c18a97b38baa672efc0c114a4c952e34eee
Aditi Reddy <aditimayareddy@gmail.com>
2022-06-11 21:05:49 -0400

    Independent Variable EDA

---
+++ helper_module.py
@@ -0,0 +1,77 @@
+#Helper module with functions for Ames Regression Project
+import pandas as pd
+import numpy as np
+import numpy.ma as ma
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+from scipy.stats import pearsonr
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+#Functions needed
+    
+
+#Function to help quickly look at a variable list of categorical features. For each feature, function gives number of missing values, pct of missing values, countplot and boxplot.
+def look_cat(varlist):
+    for var in varlist:
+        count = train[var].value_counts()
+        #info about missingness
+        num_miss = train[var].isnull().sum()
+        pct_miss = round(num_miss/train.shape[0]*100, 2)
+        print(var + ' has ' + str(num_miss) + ' missing observations, equal to ' + str(pct_miss) + '%')
+        #print(var + ' values are ' + str(train[var].unique()))
+        
+        #Figures
+        sns.set_theme(style="whitegrid")
+        med = train.groupby([var])['log_SalePrice'].median().sort_values(ascending = False).reset_index()
+
+        #seaborn Count Plot
+        f, ax = plt.subplots(figsize = (6,4))
+        fig1 = sns.countplot(x=train[var],
+                      data=train,
+                     order = list(med[var]))
+        fig1.set(xlabel = var, ylabel = 'Count')
+        
+        
+        f, ax = plt.subplots(figsize = (6,4))
+        fig = sns.boxplot(x=var,
+                          y = 'log_SalePrice',
+                          order = list(med[var]),
+                          data=train);
+        plt.xticks(rotation=45)
+        fig.set(xlabel=var, ylabel='Log of Sale Price')
+        
+        yield fig
+        
+#Function to help quickly look at a variable list of numerical features. For each feature, function gives number of missing values, pct of missing values, histogram and scatterplot.
+def look_num(varlist):
+    for var in varlist:
+        
+        #info about missingness
+        num_miss = train[var].isnull().sum()
+        pct_miss = round(num_miss/train.shape[0]*100, 2)
+        print(var + ' has ' + str(num_miss) + ' missing observations, equal to ' + str(pct_miss) + '%')
+        
+        #info about pearson's correlation
+        #corr = pearsonr(train[var], train['log_SalePrice'])
+        #print('Pearsons correlation (r):', round(corr[0],3))
+        
+        #Figures
+        sns.set_theme(style="whitegrid")
+
+        #seaborn Histogram
+        f, ax = plt.subplots(figsize = (6,4))
+        fig1 = sns.histplot(x=var,
+                      data=train)
+        fig1.set(xlabel = var, ylabel = 'Count')
+        
+        #seaborn Scatterplot
+        f, ax = plt.subplots(figsize = (6,4))
+        fig = sns.scatterplot(x=var,
+                          y = 'log_SalePrice',
+                          data=train);
+        plt.xticks(rotation=45)
+        fig.set(xlabel=var, ylabel='Log of Sale Price')
+        
+        yield fig
